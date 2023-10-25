9cef7699233416a73e749c4e30490eff0aefcc94
JWHudak <joehudak4@gmail.com>
2023-10-25 11:53:32 -0500

    3

--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         try {
-            IntVector test = new IntVector(-1000);
+            IntVector test = new IntVector(0);
+
+            for(int i = 0; i < 20; i++) {
+                test.add(i);
+            }
+
+            System.out.println(test);
+
+            test.add(0, 50);
+            System.out.println(test);
+
+
+
+
             
-            System.out.println("Size: " + test.size());
+           
         } catch(IllegalArgumentException ex) {
             System.out.println("That's illegal!");
         } catch(ArithmeticException ex) {
